cdc3d22fc283c308a7109db124d4fe17801041cf
juke01 <juke01spx@gmail.com>
2019-05-23 14:15:38 +1000

    Made <head> more consistent between pages. Still needs a bit of work. Need to specify viewport in order to work better on a 1080p display (PLDs are not in 1080p).

--- Phase1/Website/quizAnswers.php
+++ Phase1/Website/quizAnswers.php
@@ -5,10 +5,12 @@ session_unset();
 <!doctype html>
 <html>
 <head>
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <!-- POSSIBLY REMOVE OR MODIFY TAG? TEST ON 1080p DISPLAY <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
 <meta charset="utf-8">
 <title>Quiz Results - Mechanical Engineering</title>
+
 <!-- Icon for webpage -->
 <link rel="shortcut icon" href="Img/favicon.ico">
 	
@@ -19,7 +21,6 @@ session_unset();
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-	
 
 </head>
 
